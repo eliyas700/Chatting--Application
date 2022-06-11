56381332cafd34a66611a218e685cdb8982c74d0
Eliyas Hossain <eliyashossain700@gmail.com>
2022-06-11 22:23:38 +0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { GrSend } from "react-icons/gr";
+import { FaUserCircle } from "react-icons/fa";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-[90vh] w-[95%] mx-auto flex justify-between ">
+      <div className="w-[25%]"></div>
+      <div className="w-[75%] border-2 relative">
+        <h1 className="bg-primary text-white text-xl py-3 pl-3 flex  items-center">
+          User <FaUserCircle size={34} className="ml-2" />{" "}
+        </h1>
+
+        <div className="border-2 absolute bottom-0 bg-[#7f8c8d] w-[100%] py-3 px-3">
+          <form className="flex justify-between items-center">
+            <input type="text" className="w-[90%] h-[40px]" />
+            <button type="submit">
+              <GrSend size={34} className="" />{" "}
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
